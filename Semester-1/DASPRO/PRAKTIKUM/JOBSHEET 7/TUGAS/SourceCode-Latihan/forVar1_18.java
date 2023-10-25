72c4613c1e64cc9c732a353554a80fcd7def98d7
Mohammad Adri Favian <96401005+AdriFavian@users.noreply.github.com>
2023-10-25 09:27:36 +0700

    Delete Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 7/TUGAS directory

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 7/TUGAS/SourceCode-Latihan/forVar1_18.java
+++
@@ -1,7 +0,0 @@
-public class forVar1_18 {
-    public static void main(String[] args){
-        for (int i = 1, j = 10; i < j; i++, j--){
-            System.out.printf("%03d -- %03d\n", i, j);
-        }
-    }
-} 
