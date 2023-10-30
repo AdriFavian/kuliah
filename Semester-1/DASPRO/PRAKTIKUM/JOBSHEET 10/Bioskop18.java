85649208f44d1fce68f8ce8d0e48ad1126d316f0
AdriFavian <adrifavian2@gmail.com>
2023-10-30 13:37:01 +0700

    initial commit

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 10/Bioskop18.java
+++ Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 10/Bioskop18.java
@@ -13,8 +13,8 @@ public class Bioskop18 {
 
         System.out.println(penonton.length);
         
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
