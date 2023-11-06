032a2b64f906453b849ac6cd6997371da8a7c471
AdriFavian <adrifavian2@gmail.com>
2023-11-06 12:48:35 +0700

    feat: tambahan fungsi

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Star18.java
+++ Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Star18.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Star {
+public class Star18 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i > N; i--) {
             System.out.print("*");
         }
     }
