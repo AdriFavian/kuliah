a21e080a439bcf4e43e55262e025c242b53ac8e1
AdriFavian <adrifavian2@gmail.com>
2023-11-06 12:50:12 +0700

    feat(Square.java): inner loop

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Square18.java
+++ Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Square18.java
@@ -6,8 +6,10 @@ public class Square18 {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
+        for(int iOuter=1; iOuter<=N; iOuter++) {
             for(int i = 1; i <= N; i++) {
                 System.out.print("*");
             }
+        }
     }
 }
