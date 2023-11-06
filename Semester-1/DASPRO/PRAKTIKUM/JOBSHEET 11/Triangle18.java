4d08570883e2c3927c3a33e14c134cfd0d2c324d
AdriFavian <adrifavian2@gmail.com>
2023-11-06 13:30:11 +0700

    feat(jobsheet11): add Quiz18.java

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Triangle18.java
+++ Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Triangle18.java
@@ -4,16 +4,16 @@ public class Triangle18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
-        int N =sc.nextInt();
+        int N = sc.nextInt();
         int i = 0;
-
-        while(i<=N){
+        while(i <= N) {
             int j = 0;
-            while(j<i) {
+            while(j < i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
-        }
+        } 
     }
 }
