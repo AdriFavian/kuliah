6dbc11474b0d87e99406bbef25738e1e1c8b736f
AdriFavian <adrifavian2@gmail.com>
2023-11-06 14:08:01 +0700

    feat: add number checker

--- Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Quiz18.java
+++ Semester-1/DASPRO/PRAKTIKUM/JOBSHEET 11/Quiz18.java
@@ -13,6 +13,14 @@ public class Quiz18 {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
+
+                if (answer > number) {
+                    System.out.println("Tebakan Anda lebih besar dari jawaban.");
+                } else if (answer < number) {
+                    System.out.println("Tebakan Anda lebih kecil dari jawaban.");
+                } else {
+                    success = true;
+                }
                 success = (answer == number);
             } while(!success);
                 System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
