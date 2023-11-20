1eaa3ee566ae0c7a2f6ad7e4fd93a052048c2b38
AdriFavian <adrifavian2@gmail.com>
2023-11-20 14:59:23 +0700

    feat: add pesan tambahan

--- Semester-1/DASPRO/JOBSHEET 13/UcapanTerimakasih_18.java
+++ Semester-1/DASPRO/JOBSHEET 13/UcapanTerimakasih_18.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 public class UcapanTerimakasih_18 {
+
     public static String PenerimaUcapan() {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan: ");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
 
     public static void UcapanTerimakasih() {
         String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like i could ask you anything.");
-    }
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything.");
 
+        UcapanTambahan();
+    }
+    public static void UcapanTambahan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan ucapan tambahan: ");
+        String ucapanTambahan = sc.nextLine();
+        System.out.println("Additional message: " + ucapanTambahan);
+    }
     public static void main(String[] args) {
         UcapanTerimakasih();
     }
-} 
+}
