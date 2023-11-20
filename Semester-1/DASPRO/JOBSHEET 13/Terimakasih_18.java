411d1cd44746c41dbc16f62e09e763ca2b3b6585
AdriFavian <adrifavian2@gmail.com>
2023-11-20 13:52:31 +0700

    feat: modifikasi program

--- Semester-1/DASPRO/JOBSHEET 13/Terimakasih_18.java
+++ Semester-1/DASPRO/JOBSHEET 13/Terimakasih_18.java
@@ -1,15 +1,11 @@
 public class Terimakasih_18 {
-    public static void UcapanTerimakasih() {
-        System.out.println("Thank you for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like i could ask you anything.");
-    }
-
     public static void UcapanTambahan (String ucapan) {
         System.out.println(ucapan);
     }
-
+    
     public static void main(String[] args) {
-        UcapanTerimakasih();
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like i could ask you anything.");
         String ucapan = "Terimakasih Pak.. Bu.. Semoga sehat selalu";
         UcapanTambahan(ucapan);
     }
