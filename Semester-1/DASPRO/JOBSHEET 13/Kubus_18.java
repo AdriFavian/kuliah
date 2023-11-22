0340d8be3710f8d5759a2c408fd915fcc6f63d63
AdriFavian <adrifavian2@gmail.com>
2023-11-22 18:21:46 +0700

    feat: add Daspro-13 tugas

---
+++ Semester-1/DASPRO/JOBSHEET 13/Kubus_18.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Kubus_18 {
+    double sisi;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan panjang sisi kubus: ");
+        double sisiKubus = sc.nextDouble();
+        
+        double volume = hitungVolume(sisiKubus); 
+        System.out.println("Volume kubus: " + volume);
+        
+        double luasPermukaan = hitungLuasPermukaan(sisiKubus);
+        System.out.println("Luas permukaan kubus: " + luasPermukaan);
+        sc.close();
+    }
+    static double hitungVolume(double sisi) {
+        double volume = sisi * sisi * sisi;
+        return volume;
+    }
+    static double hitungLuasPermukaan(double sisi) {
+        double luasPermukaan = 6 * sisi * sisi;
+        return luasPermukaan;
+    }
+}
